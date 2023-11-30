0ba7aef765fb702feabe936a7bd9136f686c3a96
zipfiler <zipfiler1@gmail.com>
2023-12-01 02:30:34 +0300

    Migrate to postgres

--- Recipes/settings.py
+++ Recipes/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'Recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "recipes_db",
+        "USER": "recipes_username",
+        "PASSWORD": "recipes_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
