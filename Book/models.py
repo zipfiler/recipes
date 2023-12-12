94a54f467a475110b77e73133d5f228c0cb9b448
zipfiler <zipfiler1@gmail.com>
2023-12-12 18:31:52 +0300

    Before custom User

--- Book/models.py
+++ Book/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+
+
 TYPES = (
     ('Первое', 'первое'),
     ('Второе', 'второе'),
@@ -34,3 +37,32 @@ class Recipe(models.Model):
     
     def __str__(self):
         return f'{self.title}. Автор: {self.author}'
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
+
+    # def send_verification_email(self):
+    #     link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
+    #     verification_link = f'{settings.DOMAIN_NAME}{link}'
+    #     subject = f'Подтверждение учётной записи для {self.user.username}'
+    #     message = 'Для подтверждения учётной записи для {} перейдите по ссылке: {}'.format(
+    #         self.user.email,
+    #         verification_link
+    #     )
+    #     send_mail(
+    #         subject=subject,
+    #         message=message,
+    #         from_email=settings.EMAIL_HOST_USER,
+    #         recipient_list=[self.user.email],
+    #         fail_silently=False,
+    #     )
+
+    # def is_expired(self):
+    #     return now() >= self.expiration
--- Recipes/settings.py
+++ Recipes/settings.py
@@ -142,3 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Sending emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
